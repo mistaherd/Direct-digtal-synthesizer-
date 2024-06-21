c7ea7fff5edb46123d3eae4db39a0fc55ca33f72
mistaherd <95090888+mistaherd@users.noreply.github.com>
2024-06-21 19:14:55 +0100

    updated write to mf file

--- write_to_mif.py
+++ write_to_mif.py
@@ -1,10 +1,25 @@
 import numpy as np
+import sys
+def sine_wave_cycle_logic(choice:int):
+    if choice==0:
+        N=2**5
+        t=np.linspace(0,1,N)
+        fs=1
+        output= np.sin(2*np.pi*fs*t)
+        output=np.round(np.sin(2*np.pi*fs*t)*15).astype(np.uint8)
 
-N=2**5
-t=np.linspace(0,1,N)
-fs=1
-output= np.sin(2*np.pi*fs*t)
-output=np.round(np.sin(2*np.pi*fs*t)*15).astype(np.uint8)
+    elif choice==1:
+        N=2**7
+        t=np.linspace(0,1,N//2)
+        fs=1
+        output= np.sin(2*np.pi*fs*t)
+        output=np.round(output*15).astype(np.uint8)[:32]
+    else:
+        print("0: full cycle sine wave \n1: half cycle sine wave")
+        return None
+
+    return output
+output=sine_wave_cycle_logic(1)
 print(output)
 with open("C:\MWT2\DSS\coswavesoted.mif",'w') as file:
     file.write("--This has been generated from python\n")
