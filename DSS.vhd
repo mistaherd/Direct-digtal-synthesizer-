8a12140646951a9982c48b2b9c69421ccb252f64
Mistaherd <lanzbulldog22@gmail.com>
2024-10-29 01:10:50 +0000

    ?

--- DSS.vhd
+++ DSS.vhd
@@ -8,8 +8,7 @@ USE lpm.lpm_components.std_logic_2D;
 ENTITY DSS IS
 	PORT
 	(
-		datain0	:	IN	STD_LOGIC_VECTOR(4 downto 0);
-		datain1	:	IN	STD_LOGIC_VECTOR(4 downto 0);
+		datain	:	IN	STD_LOGIC_VECTOR(4 downto 0);
 		clkin		:	IN	STD_LOGIC;
 		
 		dataout	: 	OUT	STD_LOGIC_VECTOR(7 downto 0);
@@ -21,22 +20,10 @@ ENTITY DSS IS
 	SIGNAL out_ADD : STD_LOGIC_VECTOR (4 downto 0);
 	SIGNAL out_DFF : STD_LOGIC_VECTOR (4 downto 0);
 	SIGNAL out_DFF1: STD_LOGIC_VECTOR(0 downto 0);
-	SIGNAL out_DFF2: STD_LOGIC_VECTOR(0 downto 0);
+	
 	SIGNAL clkdata : STD_LOGIC_VECTOR(0 downto 0);
 	SIGNAL ROM_OUT : STD_LOGIC_VECTOR(7 downto 0);
 	
-	SIGNAL in_mux1 : STD_LOGIC_VECTOR(4 downto 0); -- Declare in_mux1
-	SIGNAL mux_sel	: STD_LOGIC_VECTOR(0 downto 0);
-	SIGNAL mux_out : STD_LOGIC_VECTOR(4 downto 0);
-	SIGNAL shift_out: STD_LOGIC_VECTOR(2 downto 0);
-	COMPONENT MUX
-				PORT(
-						in_mux0 :	in		STD_LOGIC_VECTOR(4 downto 0);
-						in_mux1 :	in		STD_LOGIC_VECTOR(4 downto 0);
-						sel :	in		STD_LOGIC_VECTOR(0 downto 0);
-						mux_out:		out	STD_LOGIC_VECTOR(4 downto 0)
-					);
-	END COMPONENT;
 	COMPONENT LPMROM
 	  PORT (
 				address : in std_logic_vector(4 downto 0);  -- Adjust address width as needed
@@ -53,7 +40,7 @@ ENTITY DSS IS
 			)
 		PORT MAP
 			(
-				dataa => datain0,
+				dataa => datain,
 				datab => out_DFF,
 				result=> out_ADD
 			);
@@ -85,23 +72,22 @@ ENTITY DSS IS
 			CLOCK =>clkin,
 			Q=>out_DFF1
 		);
-		Mux_ASK: MUX
-		PORT MAP
-		(
-			in_mux0 =>out_DFF,
-			in_mux1 =>datain1,
-			sel=>out_DFF1,
-			mux_out=>mux_out
-		);
+	
+		
+		
 		MyROM : LPMROM
 			PORT MAP
 			(
-			address => mux_out,
+			address => out_DFF,
 			clock => clkin,
-			q =>dataout
+			q =>ROM_OUT
 			);
+		Mux_ASK: MUX
+		if out_DFF1= '1' then
+			
+			dataout<=ROM_OUT;
 		
-		
+		end if
 		
 	END mycomp;
  
